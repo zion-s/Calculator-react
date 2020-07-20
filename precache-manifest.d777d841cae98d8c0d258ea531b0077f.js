a7d6d3ec6a15e16481accca415b8d69ddcc056a7
Zion Sandeep <59429929+zion-s@users.noreply.github.com>
2020-07-20 23:29:14 +0530

    Updates

---
+++ precache-manifest.d777d841cae98d8c0d258ea531b0077f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59195b9d46205b758caae47928836591",
+    "url": "/Calculator-react/index.html"
+  },
+  {
+    "revision": "fa02ffc29318ddf623da",
+    "url": "/Calculator-react/static/css/main.b8fa665f.chunk.css"
+  },
+  {
+    "revision": "d5693366c26982f0b699",
+    "url": "/Calculator-react/static/js/2.c48dcb46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-react/static/js/2.c48dcb46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa02ffc29318ddf623da",
+    "url": "/Calculator-react/static/js/main.d31e7b5f.chunk.js"
+  },
+  {
+    "revision": "81620a0f95e4202aa0a9",
+    "url": "/Calculator-react/static/js/runtime-main.d216cb0c.js"
+  }
+]);
